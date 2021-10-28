90665e56e5b84b44797963e35f2ba0899cdb3be4
ajaakol <aleksanteri.jaakola@towermedia.fi>
2021-10-28 10:59:26 +0300

    Added per-route guards

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ import EventEdit from '@/views/event/Edit.vue';
 import About from '@/views/About.vue';
 import NotFound from '@/views/NotFound.vue';
 import NetworkError from '@/views/NetworkError.vue';
+import NProgress from 'nprogress';
+import EventService from '@/services/EventService.js';
+import Gstore from '@/store';
 
 const routes = [
   {
@@ -25,6 +28,22 @@ const routes = [
     name: 'EventLayout',
     props: true,
     component: EventLayout,
+    beforeEnter: (to) => {
+      return EventService.getEvent(to.params.id)
+        .then((response) => {
+          Gstore.event = response.data;
+        })
+        .catch((error) => {
+          if (error.response && error.response.status == 404) {
+            return {
+              name: '404Resource',
+              params: { resource: 'event' },
+            };
+          } else {
+            return { name: 'NetworkError' };
+          }
+        });
+    },
     children: [
       {
         path: '',
@@ -70,6 +89,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
+});
+
+router.beforeEach(() => {
+  NProgress.start();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -28,7 +28,6 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue';
 import EventService from '@/services/EventService.js';
-import { watchEffect } from 'vue';
 
 export default {
   name: 'EventList',
@@ -42,18 +41,27 @@ export default {
       totalEvents: 0,
     };
   },
-  created() {
-    watchEffect(() => {
-      this.events = null;
-      EventService.getEvents(2, this.page)
-        .then((response) => {
-          this.events = response.data;
-          this.totalEvents = response.headers['x-total-count'];
-        })
-        .catch(() => {
-          this.$router.push({ name: 'NetworkError' });
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        next((comp) => {
+          comp.events = response.data;
+          comp.totalEvents = response.headers['x-total-count'];
         });
-    });
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' });
+      });
+  },
+  beforeRouteUpdate(routeTo) {
+    return EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        this.events = response.data;
+        this.totalEvents = response.headers['x-total-count'];
+      })
+      .catch(() => {
+        return { name: 'NetworkError' };
+      });
   },
   computed: {
     hasNextPage() {
